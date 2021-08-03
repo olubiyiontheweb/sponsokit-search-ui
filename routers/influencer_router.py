71e8c10d64f2d4a06b7f0ccc2f07b55af7afde0b
Oluwatosin Olubiyi <29473996+olubiyiontheweb@users.noreply.github.com>
2021-08-03 13:51:21 +0100

    endpoints ready for elasticsearch query

--- routers/influencer_router.py
+++ routers/influencer_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from fastapi.responses import JSONResponse
 from config.config_loader import settings
 from schema.influencer import InfluencerQuery, InfluencerSchema
@@ -8,25 +8,30 @@ router = APIRouter()
 
 
 @ router.get("/search", response_model=InfluencerSchema, status_code=status.HTTP_200_OK)
-async def search_influencer(channel_display_name: str, skip: int = 0, limit: int = 10):
+async def search_influencer(channel_display_name: str, skip: int = 0, limit: int = 10, token: str = None):
     """ 
     Search and filter a list of influencers with which endpoint.
 
-    Parameters on the path are interpreted as query parameters
+    Parameters should be sent as url parameters
     """
-    return JSONResponse(content=f"{settings.DESCRIPTION} Visit /api/v1/docs for more details.",
-                        status_code=status.HTTP_200_OK)
+    if await authenticate(token):
+        return JSONResponse(content=f"{settings.DESCRIPTION} Visit /api/v1/docs for more details.",
+                            status_code=status.HTTP_200_OK)
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
 
 
 @ router.get("/search_with_json", response_model=InfluencerSchema, status_code=status.HTTP_200_OK)
-async def search_influencer_with_json(influencer_query: InfluencerQuery):
+async def search_influencer_with_json(influencer_query: InfluencerQuery, token: str = None):
     """ 
     Search and filter a list of influencers with which endpoint.
-    Parameters should be set as json.
+    Parameters should be sent as json.
     """
 
-    if authenticate:
-        pass
-
-    return JSONResponse(content=f"{settings.DESCRIPTION} Visit /api/v1/docs for more details.",
-                        status_code=status.HTTP_200_OK)
+    if await authenticate(token):
+        return JSONResponse(content=f"{settings.DESCRIPTION} Visit /api/v1/docs for more details.",
+                            status_code=status.HTTP_200_OK)
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
